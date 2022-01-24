adc355ce31312538ee7519ed0411dcccbbb99f85
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2022-01-24 08:56:21 +0800

    sample code for swapping behavior

--- src/main/scala/dev/codefactory/akka/essentials/BankingApp.scala
+++ src/main/scala/dev/codefactory/akka/essentials/BankingApp.scala
@@ -1,18 +1,33 @@
 package dev.codefactory.akka.essentials
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 object BankingApp extends App {
 
+  object Bank {
+    case class Withdraw(amount: BigDecimal)
+    case class Deposit(amount: BigDecimal)
+    case object Statement
+
+    case class Success(msg: String)
+  }
   class Bank(var name: String, var balance: BigDecimal) extends Actor {
     override def receive: Receive = {
       case _ => println("???")
     }
   }
 
+  case class Yolo(bank: ActorRef)
   class Person(val name: String) extends Actor {
+    import Bank._
+
     override def receive: Receive = {
-      case _ => println("???")
+      case Yolo(bank) => {
+        bank ! Withdraw(1000)
+        bank ! Deposit(1000)
+        bank ! Withdraw(1)
+      }
+      case msg => println(msg)
     }
   }
 
---
+++ src/main/scala/dev/codefactory/akka/essentials/SwitchBehavior.scala
@@ -0,0 +1,57 @@
+package dev.codefactory.akka.essentials
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+object SwitchBehavior extends App {
+
+  object StatelessActorDemo {
+    case object ChangeToDefaultMode
+    case object ChangeToAltMode
+
+    case class DefaultReply(message: String)
+    case class AltReply(message: String)
+  }
+
+  class StatelessActorDemo extends Actor {
+
+    import StatelessActorDemo._
+
+    override def receive: Receive = defaultReceive
+
+    def defaultReceive: Receive = {
+      case ChangeToAltMode => context.become(alternativeReceive)
+      case ChangeToDefaultMode =>
+      case _ => sender() ! DefaultReply("OK")
+    }
+
+    def alternativeReceive: Receive = {
+      case ChangeToAltMode =>
+      case ChangeToDefaultMode => context.become(defaultReceive)
+      case _ => sender() ! AltReply("Not OK")
+    }
+  }
+
+  class Messenger extends Actor {
+    import StatelessActorDemo._
+
+    override def receive: Receive = {
+      case DefaultReply(message) => println(s"Default: $message")
+      case AltReply(message) => println(s"Alt Reply: $message")
+      case statelessActorRef: ActorRef => {
+        statelessActorRef ! "test"
+        statelessActorRef ! ChangeToAltMode
+        statelessActorRef ! "test"
+        statelessActorRef ! ChangeToDefaultMode
+        statelessActorRef ! "test"
+      }
+    }
+  }
+
+  val system = ActorSystem("test123")
+  val statelessActorDemo = system.actorOf(Props[StatelessActorDemo])
+  val messenger = system.actorOf(Props[Messenger])
+
+  messenger ! statelessActorDemo
+
+  system.terminate()
+}
